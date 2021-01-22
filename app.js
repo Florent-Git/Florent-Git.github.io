6e4d841c084e3a2487472922811a90bd473547e8
Florent Raeymaeckers <florent.ram@gmail.com>
2021-01-22 16:43:11 +0100

    New design

---
+++ app.js
@@ -0,0 +1,23 @@
+(function () {
+    const createNode = (el) => () => document.createElement(el);
+
+    const appendTo = (parent) => (child) => {
+        let p = parent.cloneNode(true);
+        p.appendChild(child);
+        return p;
+    }
+
+    const addClassToNode = (classType) => (node) => {
+        let nodeCopy = node.cloneNode(true);
+        nodeCopy.classList.add(classType);
+        return nodeCopy;
+    };
+
+    const addStackClass = (size) => addClassToNode(`fa-stack-${size}`);
+
+    const createSpanAndAdd2xStackClass = R.pipe(createNode("span"), addStackClass("2x"));
+
+    const appendTo2xStackSpan = appendTo(createSpanAndAdd2xStackClass());
+
+    const createIconAndAppendToStackSpan = (icon) => R.pipe(createNode("i"), addClassToNode("fas"), addClassToNode(icon), appendTo2xStackSpan)();
+})();
--- src/home.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    let slide_text = $(".slide-text");
-    let more_info = $(".more-info");
-    slide_text.hide();
-    more_info.hide();
-    slide_text.fadeIn(1000, () => more_info.fadeIn(1000))
-});
